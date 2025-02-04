76495dd634b4d829cc6c476ded36fedad13cb672
KEBT\ij.won <ij.won@konai.com>
2025-02-04 15:47:19 +0900

    chore: Prettier 및 ESLint 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-    return <h1 className='text-xl font-medium text-black'>Tab Guide</h1>;
+  return <h1 className="text-xl font-medium text-black">Tab Guide</h1>;
 }
 
 export default App;
