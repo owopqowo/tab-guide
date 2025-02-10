a5bce7bb9222b8fdc9aa23f1f52bb3104ee81fe3
KEBT\ij.won <ij.won@konai.com>
2025-02-10 16:12:19 +0900

    fix: key값 추가로 오류 수정

--- src/SubNav.jsx
+++ src/SubNav.jsx
@@ -5,7 +5,11 @@ function SubNav({ links }) {
     <nav className="sticky top-14.25 overflow-auto bg-white py-2 shadow-lg shadow-neutral-200/50">
       <div className="-mx-1 flex w-fit px-3">
         {links.map((link) => (
-          <a href={link.href} className="mx-1 flex-auto rounded-full px-2 py-1 text-sm whitespace-nowrap">
+          <a
+            key={link.key}
+            href={link.href}
+            className="mx-1 flex-auto rounded-full px-2 py-1 text-sm whitespace-nowrap"
+          >
             {link.label}
           </a>
         ))}
