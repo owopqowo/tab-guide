71cd47ea0fcdaddda05f6b1a435cc3b4bd44638a
owopqowo <owo9370@gmail.com>
2025-02-10 15:20:53 +0900

    feat: header에 sticky 속성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -28,28 +28,30 @@ function App() {
 
   return (
     <>
-      <h1 className="a11y-hidden">이용 안내</h1>
-      <nav className="overflow-auto border-b border-neutral-200">
-        <div className="flex w-fit px-3">
-          {tabs.map((tab, index) => {
-            return (
-              <button
-                ref={(el) => (tabRefs.current[index] = el)}
-                key={tab.key}
-                type="button"
-                className={`relative flex-auto cursor-pointer px-2 py-4 whitespace-nowrap ${
-                  activeIndex === index
-                    ? "font-bold after:absolute after:right-0 after:bottom-0 after:left-0 after:h-1 after:w-full after:bg-black after:content-['']"
-                    : ''
-                }`}
-                onClick={() => handleClick(index)}
-              >
-                {tab.label}
-              </button>
-            );
-          })}
-        </div>
-      </nav>
+      <header className="sticky top-0 bg-white">
+        <h1 className="a11y-hidden">이용 안내</h1>
+        <nav className="overflow-auto border-b border-neutral-200">
+          <div className="flex w-fit px-3">
+            {tabs.map((tab, index) => {
+              return (
+                <button
+                  ref={(el) => (tabRefs.current[index] = el)}
+                  key={tab.key}
+                  type="button"
+                  className={`relative flex-auto cursor-pointer px-2 py-4 whitespace-nowrap ${
+                    activeIndex === index
+                      ? "font-bold after:absolute after:right-0 after:bottom-0 after:left-0 after:h-1 after:w-full after:bg-black after:content-['']"
+                      : ''
+                  }`}
+                  onClick={() => handleClick(index)}
+                >
+                  {tab.label}
+                </button>
+              );
+            })}
+          </div>
+        </nav>
+      </header>
       {tabs[activeIndex].children}
     </>
   );
