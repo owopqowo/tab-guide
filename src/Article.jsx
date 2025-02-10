535b32beb1ebeed906908e0b8f68bea15845b969
KEBT\ij.won <ij.won@konai.com>
2025-02-10 15:23:33 +0900

    feat: Article 컴포넌트 추가 및 적용

---
+++ src/Article.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Article({ id, title, sections }) {
+  return (
+    <article id={id} className="p-5 first-of-type:pt-10 last-of-type:pb-10">
+      <h2 className="text-center text-3xl font-bold">{title}</h2>
+      <div className="counter">
+        {sections.map((section) => (
+          <div key={section.key} className="mt-8 before:block before:text-center before:text-2xl before:font-bold">
+            {section.items.map((item, index) => (
+              <React.Fragment key={`${section.key}-${index}`}>
+                {item.type === 'text' && (
+                  <p
+                    className="mx-auto mt-4 max-w-3xl text-left text-neutral-800"
+                    dangerouslySetInnerHTML={{ __html: item.content }}
+                  ></p>
+                )}
+                {item.type === 'image' && (
+                  <img src={item.content} alt={item.alt || ''} className="mx-auto mt-4 w-3xl rounded-3xl" />
+                )}
+              </React.Fragment>
+            ))}
+          </div>
+        ))}
+      </div>
+    </article>
+  );
+}
+
+export default Article;
