fc7f4ec2303f5fa747d2adc4918ccb22a58ace7b
owopqowo <owo9370@gmail.com>
2025-02-11 13:59:14 +0900

    chore: vite-plugin-svgr 추가

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'removeAttrs',
+              params: { attrs: '(fill|stroke)' }, // fill, stroke 속성을 제거
+            },
+          ],
+        },
+      },
+    }),
+  ],
 });
