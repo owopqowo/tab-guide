c606c45709aef198f7c6cf7304855adad13445c6
owopqowo <owo9370@gmail.com>
2025-02-11 12:27:34 +0900

    feat: 접근성 향상을 위한 attribute 추가

--- src/TabPanel.jsx
+++ src/TabPanel.jsx
@@ -1,12 +1,12 @@
 import Article from './Article';
 
-function TabPanel({ sectionsData, setSubActiveIndex }) {
+function TabPanel({ id, sectionsData, setSubActiveIndex }) {
   return (
-    <>
+    <div id={id} role="tabpanel" tabIndex="0">
       {sectionsData.map(({ id, label, sections }) => (
         <Article key={id} id={id} title={label} sections={sections} setSubActiveIndex={setSubActiveIndex} />
       ))}
-    </>
+    </div>
   );
 }
 
