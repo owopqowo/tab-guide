bb8c0d8396e3223e1eff1e37934779f77a5b0372
owopqowo <owo9370@gmail.com>
2025-02-11 09:46:08 +0900

    refactor: SubNav 위치 변경 및 active 기능 추가

--- src/TabPanel.jsx
+++ src/TabPanel.jsx
@@ -1,10 +1,8 @@
-import SubNav from './SubNav';
 import Article from './Article';
 
 function TabPanel({ sectionsData }) {
   return (
     <>
-      <SubNav links={sectionsData.map(({ id, label }) => ({ key: id, href: `#${id}`, label }))} />
       {sectionsData.map(({ id, label, sections }) => (
         <Article key={id} id={id} title={label} sections={sections} />
       ))}
